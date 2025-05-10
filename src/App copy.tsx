ef0dba09d6a48682978d00914f96c7a02fabdb43
dodfrm <dodiprj0@gmail.com>
2025-05-10 23:13:59 +0700

    little change

---
+++ src/App copy.tsx
@@ -0,0 +1,159 @@
+import AnimatedContent from "./Animations/AnimatedContent/AnimatedContent";
+import ClickSpark from "./Animations/ClickSpark/ClickSpark";
+import FadeContent from "./Animations/FadeContent/FadeContent";
+import "./App.css";
+import { Compare } from "./Component/compare";
+import FooterSection from "./Component/footer";
+import Navbar from "./Component/Navbar";
+import { OurTeam } from "./Component/Team";
+
+const testimonials = [
+  {
+    quote:
+      "Orang Yang Suka Bermain Badminton dan Ingin Menjadi Programmer Handal Namun Enggan Ngoding",
+    name: "Dodi Firmansyah",
+    designation: "3SI1-222212572",
+    src: "./img/1.jpg",
+  },
+  {
+    quote:
+      "Implementation was seamless and the results exceeded our expectations. The platform's flexibility is remarkable.",
+    name: "Wilfa Afriyani",
+    designation: "3SD1-222212915",
+    src: "./img/wilfa.jpeg",
+  },
+  {
+    quote:
+      "This solution has significantly improved our team's productivity. The intuitive interface makes complex tasks simple.",
+    name: "Titha Nur Izzah",
+    designation: "3SI1-222212898",
+    src: "./img/3.jpg",
+  },
+];
+function App() {
+  return (
+    <>
+      <ClickSpark
+        sparkColor="#8AAEE0"
+        sparkSize={15}
+        sparkRadius={15}
+        sparkCount={8}
+        duration={500}
+      >
+        <Navbar />
+        <div
+          id="home"
+          className="h-[100vh] bg-[url('./img/hero-bg.jpg')] bg-no-repeat bg-cover bg-center p-10"
+        >
+          <img src="./img/hero-icon.png" alt="penyu" className="h-1/2" />
+        </div>
+        <div
+          id="a"
+          className="min-h-[100vh] p-10 flex flex-col items-center justify-center relative"
+        >
+          {/* Background dengan mask */}
+          <div className="absolute inset-0 bg-green-200 -z-10 mask-b-from-50%"></div>
+
+          {/* Konten utama */}
+          <div className="text-center max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+            <FadeContent
+              blur={true}
+              duration={300}
+              easing="ease-in-out"
+              initialOpacity={0}
+              delay={200}
+            >
+              <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-900 mb-4 text-center">
+                Dua Wajah Pantai Kita:
+                <span className="text-blue-600"> Harapan</span> vs.
+                <span className="text-red-600"> Kenyataan</span>
+              </h2>
+            </FadeContent>
+            <p className="text-base sm:text-lg md:text-xl text-gray-700 leading-relaxed text-justify max-w-3xl mx-auto">
+              Setiap tahun,{" "}
+              <strong className="font-semibold">8 juta ton plastik</strong>{" "}
+              berakhir di lautan. Sampah plastik telah mengubah
+              <strong className="font-semibold">
+                {" "}
+                surga pantai menjadi tempat pembuangan
+              </strong>{" "}
+              dalam hitungan tahun.
+            </p>
+          </div>
+          <AnimatedContent
+            direction="vertical"
+            distance={100}
+            delay={200}
+            scale={1}
+            config={{ tension: 80, friction: 10 }}
+          >
+            <Compare
+              firstImage="./img/before.png"
+              secondImage="./img/after.png"
+              firstImageClassName="object-cover object-left-top"
+              secondImageClassname="object-cover object-left-top"
+              className="h-[230px] w-[350px] md:h-[400px] md:w-[500px]"
+              slideMode="drag"
+            />
+          </AnimatedContent>
+        </div>
+
+        {/* Section About */}
+        <div id="about" className="h-[100vh] p-10">
+          <h2 className="mb-16 text-center text-4xl font-bold text-gray-800">
+            2
+          </h2>
+
+          <AnimatedContent direction="horizontal" reverse>
+            <p>This content slides in from the left</p>
+          </AnimatedContent>
+        </div>
+
+        {/* Section Our Team */}
+        <div
+          id="our-team"
+          className="relative flex flex-col items-center justify-center px-4 py-20 overflow-hidden"
+        >
+          {/* Background decorative elements */}
+          <div className="absolute inset-0 bg-green-200 mask-b-from-60%">
+            <div className="absolute opacity-10 top-20 left-10 w-32 h-32 rounded-full bg-green-500 mix-blend-multiply"></div>
+            <div className="absolute opacity-10 bottom-20 right-10 w-40 h-40 rounded-full bg-teal-500 mix-blend-multiply"></div>
+          </div>
+
+          <FadeContent
+            blur={true}
+            duration={300}
+            easing="ease-in-out"
+            initialOpacity={0}
+            delay={200}
+          >
+            <div className="text-center max-w-3xl mx-auto">
+              <h2 className="mb-6 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-gray-800 bg-clip-text bg-gradient-to-r from-green-600 to-teal-600">
+                Tim Kreatif Kami
+              </h2>
+              <p className="text-lg sm:text-sm md:text-xl lg:text-2xl text-gray-700 font-medium mb-12">
+                Orang-Orang Yang Berada Di Balik Layar
+              </p>
+
+              <div className="w-24 h-1.5 bg-teal-500 mx-auto rounded-full"></div>
+            </div>
+          </FadeContent>
+          <AnimatedContent
+            direction="vertical"
+            distance={150}
+            delay={500}
+            scale={1}
+            config={{ tension: 80, friction: 10 }}
+          >
+            <OurTeam testimonials={testimonials} />
+          </AnimatedContent>
+        </div>
+
+        {/* Footer */}
+        <FooterSection />
+      </ClickSpark>
+    </>
+  );
+}
+
+export default App;
--- src/App.tsx
+++ src/App.tsx
@@ -1,160 +1,83 @@
-import AnimatedContent from "./Animations/AnimatedContent/AnimatedContent";
-import ClickSpark from "./Animations/ClickSpark/ClickSpark";
-import FadeContent from "./Animations/FadeContent/FadeContent";
 import "./App.css";
-import { Compare } from "./Component/compare";
-import FooterSection from "./Component/footer";
-import Navbar from "./Component/Navbar";
-import { OurTeam } from "./Component/Team";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
-const testimonials = [
-  {
-    quote:
-      "Orang Yang Suka Bermain Badminton dan Ingin Menjadi Programmer Handal Namun Enggan Ngoding",
-    name: "Dodi Firmansyah",
-    designation: "3SI1-222212572",
-    src: "./img/1.jpg",
-  },
-  {
-    quote:
-      "Implementation was seamless and the results exceeded our expectations. The platform's flexibility is remarkable.",
-    name: "Wilfa Afriyani",
-    designation: "3SD1-222212915",
-    src: "./img/wilfa.jpeg",
-  },
-  {
-    quote:
-      "This solution has significantly improved our team's productivity. The intuitive interface makes complex tasks simple.",
-    name: "Titha Nur Izzah",
-    designation: "3SI1-222212898",
-    src: "./img/3.jpg",
-  },
-];
+
 function App() {
   return (
     <>
-      <ClickSpark
-        sparkColor="#8AAEE0"
-        sparkSize={15}
-        sparkRadius={15}
-        sparkCount={8}
-        duration={500}
-      >
-        <Navbar />
-        <div
-          id="home"
-          className="h-[100vh] bg-[url('/src/assets/hero-bg.jpg')] bg-no-repeat bg-cover bg-center p-10"
+      <div className="font-sans">
+        <Parallax
+          pages={2}
+          style={{ width: "100%", height: "100%" }}
+          className="z-1 relative d-block"
         >
-          <h2 className="mt-20 text-center text-3xl font-bold text-black">
-            Home
-          </h2>
-            <img src="./img/hero-icon.png" alt="penyu" />
-        </div>
-        <div
-          id="a"
-          className="min-h-[100vh] p-10 flex flex-col items-center justify-center relative"
-        >
-          {/* Background dengan mask */}
-          <div className="absolute inset-0 bg-green-200 -z-10 mask-b-from-50%"></div>
-
-          {/* Konten utama */}
-          <div className="text-center max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-            <FadeContent
-              blur={true}
-              duration={300}
-              easing="ease-in-out"
-              initialOpacity={0}
-              delay={200}
-            >
-              <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold text-gray-900 mb-4 text-center">
-                Dua Wajah Pantai Kita:
-                <span className="text-blue-600"> Harapan</span> vs.
-                <span className="text-red-600"> Kenyataan</span>
-              </h2>
-            </FadeContent>
-            <p className="text-base sm:text-lg md:text-xl text-gray-700 leading-relaxed text-justify max-w-3xl mx-auto">
-              Setiap tahun,{" "}
-              <strong className="font-semibold">8 juta ton plastik</strong>{" "}
-              berakhir di lautan. Sampah plastik telah mengubah
-              <strong className="font-semibold">
-                {" "}
-                surga pantai menjadi tempat pembuangan
-              </strong>{" "}
-              dalam hitungan tahun.
-            </p>
-          </div>
-          <AnimatedContent
-            direction="vertical"
-            distance={100}
-            delay={200}
-            scale={1}
-            config={{ tension: 80, friction: 10 }}
-          >
-            <Compare
-              firstImage="./img/before.png"
-              secondImage="./img/after.png"
-              firstImageClassName="object-cover object-left-top"
-              secondImageClassname="object-cover object-left-top"
-              className="h-[230px] w-[350px] md:h-[400px] md:w-[500px]"
-              slideMode="drag"
+          {/* Background image */}
+          <ParallaxLayer offset={0} speed={0.7}>
+            <img
+              src="./img/hero-bg.jpg"
+              alt="background"
+              className="w-full h-screen object-cover"
             />
-          </AnimatedContent>
-        </div>
+          </ParallaxLayer>
 
-        {/* Section About */}
-        <div id="about" className="h-[100vh] p-10">
-          <h2 className="mb-16 text-center text-4xl font-bold text-gray-800">
-            2
-          </h2>
-
-          <AnimatedContent direction="horizontal" reverse>
-            <p>This content slides in from the left</p>
-          </AnimatedContent>
-        </div>
-
-        {/* Section Our Team */}
-        <div
-          id="our-team"
-          className="relative flex flex-col items-center justify-center px-4 py-20 overflow-hidden"
-        >
-          {/* Background decorative elements */}
-          <div className="absolute inset-0 bg-green-200 mask-b-from-60%">
-            <div className="absolute opacity-10 top-20 left-10 w-32 h-32 rounded-full bg-green-500 mix-blend-multiply"></div>
-            <div className="absolute opacity-10 bottom-20 right-10 w-40 h-40 rounded-full bg-teal-500 mix-blend-multiply"></div>
-          </div>
-
-          <FadeContent
-            blur={true}
-            duration={300}
-            easing="ease-in-out"
-            initialOpacity={0}
-            delay={200}
-          >
-            <div className="text-center max-w-3xl mx-auto">
-              <h2 className="mb-6 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-gray-800 bg-clip-text bg-gradient-to-r from-green-600 to-teal-600">
-                Tim Kreatif Kami
-              </h2>
-              <p className="text-lg sm:text-sm md:text-xl lg:text-2xl text-gray-700 font-medium mb-12">
-                Orang-Orang Yang Berada Di Balik Layar
-              </p>
-
-              <div className="w-24 h-1.5 bg-teal-500 mx-auto rounded-full"></div>
+          {/* Penyu - positioned right */}
+          <ParallaxLayer offset={0} speed={0.4}>
+            <div className="absolute right-4 md:right-10 bottom-1/4 md:bottom-1/2 transform translate-y-1/2 w-1/2 md:w-1/4">
+              <img src="./img/12.png" alt="penyu" className="w-full" />
             </div>
-          </FadeContent>
-          <AnimatedContent
-            direction="vertical"
-            distance={150}
-            delay={500}
-            scale={1}
-            config={{ tension: 80, friction: 10 }}
-          >
-            <OurTeam testimonials={testimonials} />
-          </AnimatedContent>
-        </div>
+          </ParallaxLayer>
 
-        {/* Footer */}
-        <FooterSection />
-      </ClickSpark>
+          {/* sampah */}
+          <ParallaxLayer offset={0} speed={-0.3}>
+            <div className="absolute left-4 md:left-10 bottom-1/3 md:bottom-1/2 transform translate-y-1/2 w-1/2 md:w-1/4">
+              <img src="./img/13.png" alt="sampah" className="w-2/3" />
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0} speed={0.3}>
+            <div className="absolute top-36 md:top-10 left-5/6 md:left-3/4 transform -translate-x-1/2 w-1/2 md:w-1/4">
+              <img src="./img/botol.png" alt="sampah" className="w-1/2" />
+            </div>
+          </ParallaxLayer>
+
+          {/* Ikan - positioned center bottom */}
+          <ParallaxLayer offset={0} speed={0.5}>
+            <div className="absolute top-1 left-0 w-1/2 md:w-1/4">
+              <img src="./img/14.png" alt="ikan" className="w-full" />
+            </div>
+          </ParallaxLayer>
+
+          {/* Coral - positioned bottom */}
+          <ParallaxLayer offset={0} speed={0.7}>
+            <div className="absolute bottom-0">
+              <img
+                src="./img/coral.png"
+                alt="coral"
+                className="w-full object-cover"
+              />
+            </div>
+          </ParallaxLayer>
+
+          {/* Hero Text Section */}
+          <ParallaxLayer offset={0} speed={0.2}>
+            <div className="absolute z-0 inset-0 flex flex-col items-center justify-center text-center px-4">
+              <h1 className="text-blue-300 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold drop-shadow-lg">
+                Selamatkan Laut Kita
+              </h1>
+              <p className="mt-4 text-blue-300 text-base sm:text-lg md:text-xl max-w-xl drop-shadow-md">
+                Bergabunglah dalam misi mengurangi sampah plastik dan menjaga
+                ekosistem laut untuk generasi masa depan.
+              </p>
+            </div>
+          </ParallaxLayer>
+
+          {/* Section A */}
+          <ParallaxLayer offset={0.9999999} speed={0.7}>
+            <div className="min-h-screen flex flex-col items-center justify-center relative bg-[#f9e1c2]">
+              <p>Section A</p>
+            </div>
+          </ParallaxLayer>
+        </Parallax>
+      </div>
     </>
   );
 }
